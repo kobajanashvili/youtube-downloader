ec0caa55f803c24efe7b11bd30d8efc2f9942c71
kobajanashvili <kobajanashvili@gmail.com>
2019-02-26 16:52:43 +0200

    test with download

--- app.js
+++ app.js
@@ -14,14 +14,17 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'videos')));
 
 app.get('/', (req, res) => {
-    res.sendfile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 })
+
+app.get('/aaa', (req, res) => res.download('./videos/text.txt'));
 
 
 app.post('/download', async (req, res) => {
     try {
-        // A02s8omM_hI
+        // https://www.youtube.com/watch?v=SNLJ4S-ltYM
         const info = await getInfoVideo(req.body.url.replace('http://www.youtube.com/watch?v=', ''));
+
         ytdl(req.body.url)
             .pipe(fs.createWriteStream(`videos/${info.title}.mp4`))
             .on('finish', () => res.status(200).json({
