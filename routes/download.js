be3af7f18d617066dcf52f0280e308d199afb329
kobajanashvili <kobajanashvili@gmail.com>
2019-03-14 12:41:22 +0200

    test download

--- routes/download.js
+++ routes/download.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
+const rootDir = require('../util/path');
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 const {promisify} = require('util');
 const url = require('url');
-
 const getInfoVideo = promisify(ytdl.getInfo);
 
-const router = express.Router();
 
+const router = express.Router();
 
 router.post('/download', async (req, res) => {
     try {
@@ -16,18 +16,44 @@ router.post('/download', async (req, res) => {
         const info = await getInfoVideo(req.body.url.replace('http://www.youtube.com/watch?v=', ''));
 
         ytdl(req.body.url)
-            .pipe(fs.createWriteStream(`videos/${info.title}.mp4`))
-            .on('finish', () => res.redirect(url.format({
-                pathname:"/ttt",
-                    query: {
-                       "title": `${info.title}.mp4`
-                    }
-            })))
+            .pipe(fs.createWriteStream(path.json(__dirname, `videos/${info.title}.mp4`)))
+            .on('finish', () => {
+                res.download(path.join(__dirname, './videos', `${info.title}.mp4`));
+            })
+
+
+            // .on('finish', () => res.redirect(url.format({
+            //     pathname:"/ttt",
+            //         query: {
+            //            "title": `${info.title}.mp4`
+            //         }
+            // })))
             // .on('finish', () => res.download(path.join(__dirname, './videos', `${info.title}.mp4`)));
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
+
+
+// router.post('/download', async (req, res) => {
+//     try {
+//         // https://www.youtube.com/watch?v=SNLJ4S-ltYM
+//         const info = await getInfoVideo(req.body.url.replace('http://www.youtube.com/watch?v=', ''));
+
+//         ytdl(req.body.url)
+//             .pipe(fs.createWriteStream(`videos/${info.title}.mp4`))
+//             .on('finish', () => res.redirect(url.format({
+//                 pathname:"/ttt",
+//                     query: {
+//                        "title": `${info.title}.mp4`
+//                     }
+//             })))
+//             // .on('finish', () => res.download(path.join(__dirname, './videos', `${info.title}.mp4`)));
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 
 
