9d3d38c7fe9ad6a66a9bf618b38d0cb7686be75d
kobajanashvili <kobajanashvili@gmail.com>
2019-03-16 16:58:20 +0200

    Update download.js

--- routes/download.js
+++ routes/download.js
@@ -9,25 +9,19 @@ const getInfoVideo = promisify(ytdl.getInfo);
 const router = express.Router();
 
 router.post('/download', async (req, res) => {
-    if(!req.body.url){
-        res.status(404).json({
-            message: 'url is empty!'
-        })
-    } else {
-        try {
-            // https://www.youtube.com/watch?v=SNLJ4S-ltYM
-            const info = await getInfoVideo(req.body.url.replace('http://www.youtube.com/watch?v=', ''));
-            console.log(req.body.url);
-    
-            ytdl(req.body.url)
-                .pipe(fs.createWriteStream(`./videos/${info.title}.mp4`))
-                .on('finish', () => {
-                    res.status(203).download(`./videos/${info.title}.mp4`);
-                    console.log('done! download');
-                })
-        } catch (err) {
-                res.status(500).json(err);
-        }
+    try {
+        // https://www.youtube.com/watch?v=SNLJ4S-ltYM
+        const info = await getInfoVideo(req.body.url.replace('http://www.youtube.com/watch?v=', ''));
+        console.log(req.body.url);
+
+        ytdl(req.body.url)
+            .pipe(fs.createWriteStream(`./videos/${info.title}.mp4`))
+            .on('finish', () => {
+                res.status(203).download(`./videos/${info.title}.mp4`);
+                console.log('done! download');
+            })
+    } catch (err) {
+            res.status(500).json(err);
     }
 });
 
