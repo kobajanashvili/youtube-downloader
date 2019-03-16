f6c6e51940c355037d4ec94231e5dd27f00bab94
kobajanashvili <kobajanashvili@gmail.com>
2019-03-16 16:26:21 +0200

    Update download.js
    work with save file

--- routes/download.js
+++ routes/download.js
@@ -1,11 +1,10 @@
 const express = require('express');
-
+const path = require('path');
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 const {promisify} = require('util');
 const url = require('url');
 const getInfoVideo = promisify(ytdl.getInfo);
-
 
 const router = express.Router();
 
@@ -16,46 +15,17 @@ router.post('/download', async (req, res) => {
         console.log(req.body.url);
 
         ytdl(req.body.url)
-            .pipe(fs.createWriteStream(`../videos/${info.title}.mp4`))
+            .pipe(fs.createWriteStream(`./videos/${info.title}.mp4`))
+            // .on('finish', () => {
+            //     res.redirect('http://google.ru', 200);
+            // })
             .on('finish', () => {
-                res.status(203).download(path.join(__dirname, 'videos', `${info.title}.mp4`));
+                res.status(203).download(`./videos/${info.title}.mp4`);
                 console.log('done! download');
             })
-
-            // .on('finish', () => res.redirect(url.format({
-            //     pathname:"/ttt",
-            //         query: {
-            //            "title": `${info.title}.mp4`
-            //         }
-            // })))
-            // .on('finish', () => res.download(path.join(__dirname, './videos', `${info.title}.mp4`)));
     } catch (err) {
-        res.status(500).json(err);
+            res.status(500).json(err);
     }
 });
-
-
-
-// router.post('/download', async (req, res) => {
-//     try {
-//         // https://www.youtube.com/watch?v=SNLJ4S-ltYM
-//         const info = await getInfoVideo(req.body.url.replace('http://www.youtube.com/watch?v=', ''));
-
-//         ytdl(req.body.url)
-//             .pipe(fs.createWriteStream(`videos/${info.title}.mp4`))
-//             .on('finish', () => res.redirect(url.format({
-//                 pathname:"/ttt",
-//                     query: {
-//                        "title": `${info.title}.mp4`
-//                     }
-//             })))
-//             // .on('finish', () => res.download(path.join(__dirname, './videos', `${info.title}.mp4`)));
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
-
-
-
 
 module.exports = router;
